7423c7ab6aa76e7146a842c66ecd22a3bc47d800
asutinl <austinnlee0@gmail.com>
2024-12-05 20:18:16 -0800

    Update html format
    - Update format to use bootswatch themes since we have to

--- controller/salesPerformance.js
+++ controller/salesPerformance.js
@@ -1,16 +1,8 @@
 
 //pattern 2
 const db = require('../dbConfig');
-// getProjectDetails is responsible for retrieving project details based on a given project name.
+// getSalesPerformance is responsible for retrieving 
 const getSalesPerformance = (req, res) => {
-
-    /* 
-        TO DO:
-        Find out what query does 
-        Change const projectName variable to match the query
-        Apply query; Also consider looking at query to see if that type of query
-                     is needed to match the pattern
-    */
 
     const total_units_sold = req.query.total_units_sold; // Get total_units_sold from query parameter
 
@@ -39,7 +31,7 @@ const getSalesPerformance = (req, res) => {
         res.json(results);
     });
 };
-// Export an object containing the getProjectDetails function from the project.js file. 
+// Export an object containing the getSalesPerformance function from the project.js file. 
 module.exports = {
     getSalesPerformance,
 };
